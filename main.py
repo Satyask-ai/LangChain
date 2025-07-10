db9de2a00abadf28dbb27fa020a81144977da1b6
satyas333 <princesatya333@gmail.com>
2025-07-09 19:23:30 -0500

    Final_Code

---
+++ main.py
@@ -0,0 +1,25 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import os
+
+#load openai api key from .env file
+load_dotenv()
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+#Basic prompt template
+prompt = PromptTemplate.from_template("what can you tell me about {topic}?")
+
+#set up the LLM
+llm = ChatOpenAI(temperature=0.7, model="gpt-4o-mini")
+
+#combine the LLM with the prompt template to create a chain
+#This chain will take a topic as input and return a response from the LLM
+chain = prompt | llm
+
+#Run the chain 
+response = chain.invoke({"topic":"black holes"})
+
+#Print the response
+print("\nResponse from the LLM:\n", response.content)
